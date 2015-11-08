85af2c3d615ea3f07a2b0590e7b06c4cbc4ea69f
montagy <jencean@gmail.com>
2015-11-08 09:43:18 +0800

    surface 是cpu加载，　texture是gpu加载 左上角为坐标原点 绘图函数放在clear 与present之间

--- sdlGameDevelopment.hs
+++ sdlGameDevelopment.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 import SDL
 import Control.Concurrent (threadDelay)
 import Linear
+import Linear.Affine
 import Foreign.C.Types
 
 main :: IO ()
@@ -11,10 +13,22 @@ main = do
     window <- createWindow "你好" defaultWindow {
             windowPosition = Centered
         }
-    showWindow window
+
     render <- createRenderer window (-1 :: CInt) defaultRenderer
+
+    surface <- loadBMP "assets/foo.bmp"
+    texture <- createTextureFromSurface render surface
+    freeSurface surface
+
+    TextureInfo {..} <- queryTexture texture
+
     rendererDrawColor render $= V4 0 0 0 255
     clear render
+    copy render texture
+        -- source rect
+        (Just (Rectangle (P (V2 0 0)) (V2 textureWidth textureHeight)))
+        --destination rect
+        (Just (Rectangle (P (V2 0 0)) (V2 textureWidth textureHeight)))
     present render
 
     threadDelay 1000000
