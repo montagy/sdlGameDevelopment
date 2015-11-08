d9fcb8165a52828b6391016e48e24a0a18b54c1a
montagy <jencean@gmail.com>
2015-11-08 16:50:40 +0800

    测试使用SDL_Image 因为在hackage上的sdl_image是sdl1.3版本的，不方便使用 我尝试了下移植，发现不懂的点很多 花了一个下午学习c -> haskell，对指针的使用和几个Functor的使用还没理清 搜索了下在/r/haskellgamedev上找到了sbidin的移植 github.com/sbidin/sdl2-image 感谢作者，不仅是方便了使用，还让我学到了很多

--- sdlGameDevelopment.hs
+++ sdlGameDevelopment.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
 import SDL
+import qualified SDL.Image as IM
 import Control.Concurrent (threadDelay)
 import Linear
 import Linear.Affine
@@ -16,40 +17,15 @@ main = do
 
     render <- createRenderer window (-1 :: CInt) defaultRenderer
 
-    surface <- loadBMP "assets/animation.bmp"
+    surface <- IM.load "assets/spritelib_gpl/fishdish/fishdish_mockup.png"
     texture <- createTextureFromSurface render surface
     freeSurface surface
 
-    --TextureInfo {..} <- queryTexture texture
+    clear render
+    copy render texture Nothing Nothing
+    present render
 
-    let
-        spriteSize = V2 64 205
-        clip1 = Rectangle (P (V2 0 0)) spriteSize
-        clip2 = Rectangle (P (V2 64 0)) spriteSize
-        clip3 = Rectangle (P (V2 128 0)) spriteSize
-        clip4 = Rectangle (P (V2 192 0)) spriteSize
-    rendererDrawColor render $= V4 0 0 0 255
-    let
-        loop [] = return ()
-        loop (frame : frames) = do
-            clear render
-            copyEx render texture
-                -- source rect
-                (Just frame)
-                --destination rect
-                (Just (Rectangle (P (V2 0 0)) spriteSize))
-                --angle in degree
-                0.0
-                -- point of rotation
-                Nothing
-                -- flip direction
-                (V2 True False)
-            present render
-            threadDelay 300000
-            loop frames
-
-    loop [clip1, clip2, clip3, clip4]
-    threadDelay 1000000
+    threadDelay 3000000
     destroyRenderer render
     destroyWindow window
     quit
