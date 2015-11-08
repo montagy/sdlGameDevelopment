5ab98090649a751255df7495379d7c036ffb05a2
montagy <jencean@gmail.com>
2015-11-08 21:01:06 +0800

    第三章开头讲到向量 用向量可以方便的表示位置，速度，加速度等元素 学习下vector和linear库"

--- sdlGameDevelopment.hs
+++ sdlGameDevelopment.hs
@@ -9,6 +9,14 @@ import Linear.Affine
 import Foreign.C.Types
 import Control.Monad (when, forever)
 
+spriteSize :: V2 CInt
+spriteSize = V2 64 205
+
+clip1, clip2, clip3, clip4 :: Rectangle CInt
+clip1 = Rectangle (P (V2 0 0)) spriteSize
+clip2 = Rectangle (P (V2 64 0)) spriteSize
+clip3 = Rectangle (P (V2 128 0)) spriteSize
+clip4 = Rectangle (P (V2 192 0)) spriteSize
 main :: IO ()
 main = do
     initializeAll
@@ -18,15 +26,20 @@ main = do
 
     render <- createRenderer window (-1 :: CInt) defaultRenderer
 
-    surface <- IM.load "assets/spritelib_gpl/fishdish/fishdish_mockup.png"
-    texture <- createTextureFromSurface render surface
-    freeSurface surface
+    texture <- IM.loadTexture render "assets/animation.bmp"
+    {-texture <- createTextureFromSurface render surface-}
+    {-freeSurface surface-}
 
-    forever $ do
-        clear render
-        copy render texture Nothing Nothing
-        present render
+    let
+        loop [] _ = return ()
+        loop (frame:frames) velocity = do
+                clear render
+                copy render texture (Just frame) $
+                    Just (Rectangle (P (V2 (0+velocity) 0)) spriteSize)
+                present render
+                loop frames (velocity+1)
 
+    loop (cycle ([clip1, clip2, clip3, clip4] >>= replicate 4)) 0
     threadDelay 3000000
     destroyRenderer render
     destroyWindow window
