58023ee3142676e045b6e640130f13dbe2799240
montagy <jencean@gmail.com>
2015-11-10 23:36:26 +0800

    Revert "重构了代码，但并没有明显的理清ＦＲＰ的运用"
    This reverts commit 8dfe61dd0100cb5ee3de735c9698914973b91a0e.

--- sdlGameDevelopment.hs
+++ sdlGameDevelopment.hs
@@ -14,12 +14,10 @@ import Data.Foldable
 import Data.Maybe
 import qualified Reactive.Banana as B
 import qualified Reactive.Banana.Frameworks as B
-import Data.Word
 
 spriteSize :: V2 CInt
 spriteSize = V2 64 205
-primer :: Point V2 CInt
-primer = P (V2 0 0)
+
 clip1, clip2, clip3, clip4 :: Rectangle CInt
 clip1 = Rectangle (P (V2 0 0)) spriteSize
 clip2 = Rectangle (P (V2 64 0)) spriteSize
@@ -35,70 +33,39 @@ main = do
     render <- createRenderer window (-1 :: CInt) defaultRenderer
     texture <- IM.loadTexture render "assets/animation.bmp"
 
-    source <- B.newAddHandler
-    network <- B.compile $ makeNetwork source render texture
+    (onMouseClick, fireMouseClick) <- B.newAddHandler
+    network <- B.compile $ makeNetwork onMouseClick render texture
     B.actuate network
-
-    drawPic render texture (Just primer)
-    eventLoop source
+    eventLoop fireMouseClick
 
     destroyRenderer render
     destroyWindow window
     quit
 
-drawPic :: Renderer -> Texture -> Maybe (Point V2 CInt) -> IO ()
+drawPic :: Renderer -> Texture -> MouseButtonEventData -> IO ()
 drawPic render texture mData = do
     clear render
-    copy render texture (Just clip1) (Just $ Rectangle destPoint spriteSize)
+    copy render texture (Just clip1) $
+        Just (Rectangle (fromIntegral <$> mouseButtonEventPos mData) spriteSize)
     present render
-        where
-            destPoint = case mData of
-                          Nothing -> primer
-                          Just p -> p
 
-makeNetwork :: EventSource Event -> Renderer -> Texture ->  B.MomentIO ()
-makeNetwork source render texture = do
-    eM <- B.fromAddHandler $ addHandler source
-    let eMouseClick = B.filterE getMouseClick eM
-    B.reactimate $ drawPic render texture <$> getMouseClickPos <$> eMouseClick
+makeNetwork :: B.AddHandler MouseButtonEventData -> Renderer -> Texture ->  B.MomentIO ()
+makeNetwork mClick render texture = do
+    eM <- B.fromAddHandler mClick
+    B.reactimate $ drawPic render texture <$> eM
 
-getMouseClickPos :: Event -> Maybe (Point V2 CInt)
-getMouseClickPos e = case eventPayload e of
-                       MouseButtonEvent m -> Just $ fromIntegral <$> mouseButtonEventPos m
-                       _ -> Nothing
-getMouseClick :: Event -> Bool
-getMouseClick e =
-    case eventPayload e of
-        MouseButtonEvent m -> if mouseButtonEventMotion m == Pressed
-                                then True
-                                else False
-        _ -> False
-
-eventLoop :: EventSource Event -> IO ()
-eventLoop source = loop
+eventLoop :: B.Handler MouseButtonEventData -> IO ()
+eventLoop fireClick = loop
     where loop = do
-            e <- collectEvent
-            case eventPayload e of
-              QuitEvent -> return ()
-              _ -> fire source e >> loop
+            e <- pollEvent
+            case eventPayload <$> e of
+                Nothing -> loop
+                Just e' -> case e' of
+                                QuitEvent -> return ()
+                                MouseButtonEvent m  -> if mouseButtonEventMotion m== Pressed
+                                                                then fireClick m >> loop
+                                                                else loop
+                                _ -> loop
 
-{-----------------------------
- 尝试结合sdl和banana
-------------------------------}
-collectEvent :: IO Event
-collectEvent = do
-    e <- pollEvent
-    case e of
-      Nothing -> collectEvent
-      Just e' -> return e'
 
-type EventSource a = (B.AddHandler a, B.Handler a)
-type SDLEventSource = EventSource Event
-type SDLEvent = B.Event Event
-
-addHandler :: EventSource a -> B.AddHandler a
-addHandler = fst
-
-fire :: EventSource a -> B.Handler a
-fire = snd
 
